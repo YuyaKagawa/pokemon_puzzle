bb1de24c76110fbd5872cec4c9a57601eaa5bb77
YuyaKagawa <kagawa.yuya.k@gmail.com>
2021-03-12 18:24:46 +0900

    20210312
    ・console.logの追加

--- docs/drawing.js
+++ docs/drawing.js
@@ -76,7 +76,7 @@ function get_csv(args) {
 
                 for (let i=0;i<snum;i++){
                     for (let j=0;j<snum;j++){
-                        console.log(`result3 = ${result}`);
+                        console.log(`result[${i}] = ${result[i]}`);
                         console.log(`result[${i}][${j}] = ${result[i][j]}`);
 
                         let c = result[i][j].charCodeAt(0);
